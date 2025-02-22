58bc71041b347efc22bb8e943f9128a14b605e28
Carlos Fontes e Sousa <cetfsousa@gmail.com>
2025-02-22 12:00:42 +0100

    shows page info only if tableRows.length > 0

--- src/app/firmware/page.tsx
+++ src/app/firmware/page.tsx
@@ -207,6 +207,7 @@ export default function Firmware() {
         )}
       </tbody>
     </table>
+    {tableRows.length > 0 && (
     <div className="flex justify-between my-4">
       <button
         onClick={prevPage}
@@ -226,6 +227,7 @@ export default function Firmware() {
         Next
       </button>
     </div>
+   ) }
     {showModal && (
       <div className="fixed inset-0 flex items-center justify-center bg-black bg-opacity-50">
         <div className="bg-white p-4 w-3/4">
